fad38ae5b1c0958f25e1c9e3dbc559b7b06b32cf
Cory Bradbury <coradabra@gmail.com>
2023-10-25 17:41:41 +1100

    Complete game logic and adaptive styling
    Fix issues with wide screen overflowing down the page by adding media queries
    
    Add shading to pizels to change by 10% rather then going black immediately
    
    Add additional margin for wide screen spacing of elements

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # etch-a-sketch
+
+Basic etch-a-stech game
+
+Hover your mouse over the tiles to change the colours and make a picture.
+
+Project idea curtosy of the Odin Project.
--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const gameBoard = document.querySelector(".game-board");
 const gameBoardFragment = document.createDocumentFragment();
+const resetBtn = document.querySelector(".controls button");
 
-let gridSize = 16;
+window.addEventListener("resize", () => {
+  console.log(window.matchMedia("(max-width: 700px)"));
+});
 
-function setGameBoard() {
+resetBtn.addEventListener("click", () => {
+  const gridSize = +prompt("What size grid would you like? (1-100)");
+
+  if (!gridSize || gridSize > 100 || gridSize < 1) {
+    alert("Invalid value entered. Please select a number between 1 and 100.");
+    return;
+  }
+
+  setGameBoard(gridSize);
+});
+
+function setGameBoard(gridSize = 16) {
+  gameBoard.innerHTML = "";
+
   for (i = 0; i < gridSize * gridSize; i++) {
     const tile = document.createElement("div");
     tile.classList.add("tile");
-    tile.style.width = `${90 / gridSize}vw`;
-    tile.style.height = `${90 / gridSize}vw`;
 
-    tile.addEventListener(
-      "mouseover",
-      () => (tile.style.opacity = tile.style.opacity - 0.1)
-    );
+    tile.style.width = 100 / gridSize + "%";
+    tile.style.height = 100 / gridSize + "%";
+
+    tile.addEventListener("mouseover", () => {
+      const currentOpacity = getComputedStyle(tile).getPropertyValue("opacity");
+      tile.style.opacity = (+currentOpacity - 0.1).toString();
+    });
     gameBoardFragment.appendChild(tile);
   }
   gameBoard.appendChild(gameBoardFragment);
